d51353ef209b329cad0a249c174e2cfea03aacaa
NataliaIlina <metlinova86@gmail.com>
2019-05-03 14:57:12 +0300

    Обновляет настройку webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ module.exports = {
   entry: `./src/index.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
+    path: path.join(__dirname, `public`)
   },
   devServer: {
     contentBase: path.join(__dirname, `public`),
     compress: false,
     port: 8080,
+    open: true
   },
   module: {
     rules: [
@@ -17,10 +18,17 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: `babel-loader`,
-        },
-      },
-    ],
+          loader: `babel-loader`
+        }
+      }
+    ]
   },
-  devtool: `source-map`,
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, `src/`),
+      components: path.resolve(__dirname, `src/components/`)
+    },
+    extensions: [`.js`, `.jsx`]
+  },
+  devtool: `source-map`
 };
