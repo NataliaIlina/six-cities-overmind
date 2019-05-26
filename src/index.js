430e711fdc99be1f80f69dbb783a1b223c913320
NataliaIlina <metlinova86@gmail.com>
2019-05-26 09:55:57 +0300

    Добавляет Redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {reducer} from "./reducer";
 import {Provider} from "react-redux";
 
 const init = () => {
-  const store = createStore(reducer);
+  const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   ReactDOM.render(
       <Provider store={store}>
         <App />
