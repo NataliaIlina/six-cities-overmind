2c06032bae1bc50e0d4b3087d16741381cde85f3
NataliaIlina <metlinova86@gmail.com>
2019-05-26 11:42:42 +0300

    Добавляет HOC withActiveItem

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -1,35 +1,49 @@
 import React from "react";
 import PropTypes from "prop-types";
+import withActiveItem from "src/hocs/with-active-item/with-active-item";
 
-const CitiesList = ({onCityChange, currentCity, cities}) => (
-  <div className="cities tabs">
-    <section className="locations container">
-      <ul className="locations__list tabs__list">
-        {cities.map((city) => (
-          <li className="locations__item" key={city}>
-            <a
-              className={`locations__item-link tabs__item ${
-                currentCity === city ? `tabs__item--active` : ``
-              }`}
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                onCityChange(city);
-              }}
-            >
-              <span>{city}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </section>
-  </div>
-);
+class CitiesList extends React.PureComponent {
+  componentDidMount() {
+    this.props.setActiveItem(this.props.currentCity);
+  }
+
+  render() {
+    const {cities, activeItem, setActiveItem, onCityChange} = this.props;
+
+    return (
+      <div className="cities tabs">
+        <section className="locations container">
+          <ul className="locations__list tabs__list">
+            {cities.map((city) => (
+              <li className="locations__item" key={city}>
+                <a
+                  className={`locations__item-link tabs__item ${
+                    activeItem === city ? `tabs__item--active` : ``
+                  }`}
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setActiveItem(city);
+                    onCityChange(city);
+                  }}
+                >
+                  <span>{city}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
 
 CitiesList.propTypes = {
   onCityChange: PropTypes.func.isRequired,
   currentCity: PropTypes.string.isRequired,
-  cities: PropTypes.arrayOf(PropTypes.string).isRequired
+  cities: PropTypes.arrayOf(PropTypes.string).isRequired,
+  activeItem: PropTypes.any,
+  setActiveItem: PropTypes.func.isRequired
 };
 
-export default CitiesList;
+export default withActiveItem(CitiesList);
--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -1,36 +1,21 @@
 import React from "react";
 import PlaceCard from "components/place-card/place-card";
 import PropTypes from "prop-types";
+import withActiveItem from "src/hocs/with-active-item/with-active-item";
 
-class OffersList extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeCard: null
-    };
-    this._setActiveCard = this._setActiveCard.bind(this);
-  }
-
-  _setActiveCard(offer) {
-    this.setState({activeCard: offer});
-  }
-
-  render() {
-    const {offers} = this.props;
-    return (
-      <div className="cities__places-list places__list tabs__content">
-        {offers.map((offer, index) => (
-          <PlaceCard
-            offer={offer}
-            key={`${offer.name}_${index}`}
-            onCardClick={this._setActiveCard}
-            onCardHover={this._setActiveCard}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const OffersList = ({offers, setActiveItem, activeItem}) => (
+  <div className="cities__places-list places__list tabs__content">
+    {offers.map((offer, index) => (
+      <PlaceCard
+        offer={offer}
+        activeOffer={activeItem}
+        key={`${offer.name}_${index}`}
+        onCardClick={setActiveItem}
+        onCardHover={setActiveItem}
+      />
+    ))}
+  </div>
+);
 
 OffersList.propTypes = {
   offers: PropTypes.arrayOf(
@@ -43,7 +28,9 @@ OffersList.propTypes = {
         url: PropTypes.string,
         coords: PropTypes.arrayOf(PropTypes.number)
       })
-  ).isRequired
+  ).isRequired,
+  activeItem: PropTypes.any,
+  setActiveItem: PropTypes.func.isRequired
 };
 
-export default OffersList;
+export default withActiveItem(OffersList);
